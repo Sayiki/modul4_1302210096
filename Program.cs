39ed00ba715bf43cdbb238352969b783b146f179
Sayiki <arzaqajradika1@gmail.com>
2023-03-15 16:22:27 +0700

    menambahkan class KodeBuah

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Xml.Serialization;
+
+namespace modul4_1302210096;
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodeBuah codeBuah = new KodeBuah();
+        KodeBuah.Buah buah = KodeBuah.Buah.Apel;
+        String kode = KodeBuah.getKodeBuah.GetKodeBuah(buah);
+        Console.WriteLine(buah + " " + kode);
+    }
+}
